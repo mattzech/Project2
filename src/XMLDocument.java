0cecca45ad7b8ed1b1ad02a364fb40feb7895659
mattzech <48557999+mattzech@users.noreply.github.com>
2019-03-20 13:38:44 +0100

    Update XMLDocument.java

--- src/XMLDocument.java
+++ src/XMLDocument.java
@@ -7,22 +7,44 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import java.io.File;
 import java.io.IOException;
 
+
+/**
+ * This class creates a new XML document of a
+ * Shakespeare play and parses it, asking for user input
+ * and rewriting it into a new file
+ * @author James Stevenson and Matt Zech
+ *
+ */
 public class XMLDocument {
     Document document;
     Element root;
+	private String file;
 
+	
+	/**
+	 * Importing XML Doc
+	 * @param file
+	 */
     XMLDocument(File file) {
         setXMLDocument(file);
     }
-
+    /**
+     * Creates new XML document
+     * @param file
+     */
     public void setXMLDocument(File file) {
+    	this.file = "hamlet.xml";
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
-            document = builder.parse(file);
+            document = builder.parse(new File(this.file));
             root = document.getDocumentElement();
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
@@ -32,13 +54,22 @@ public class XMLDocument {
             e.printStackTrace();
         }
     }
+    /**
+     * Finds number of different characters in play
+     * @return integer representing speakers in the play
+     */
 
     public int findNumberOfPersona() {
         NodeList personaList = root.getElementsByTagName("PERSONA");
         int numPersona = personaList.getLength();
         return numPersona;
     }
-
+	/**
+	 * Finds how many times the user-specified speaker
+	 * talks during the play
+	 * @param checkedName automatically converts it to upper case
+	 * @return number of lines in the play
+	 */
     public int findNumberOfSpeaker(String checkedName) {
         int count = 0;
         NodeList speakerList = root.getElementsByTagName("SPEAKER");
@@ -50,7 +81,11 @@ public class XMLDocument {
         }
         return count;
     }
-
+    /**
+     * Searches the play for a user-specified word or phrase
+     * @param fragment
+     * @return list of lines where the phrase is spoken
+     */
     public SearchResult findLine(String fragment) {
         if (fragment.equals(""))
             fragment = "To be, or not to be";
@@ -71,10 +106,42 @@ public class XMLDocument {
         SearchResult result = new SearchResult(lineNodeArray, (lEndTime - lStartTime) / 1000000000.0, numberOfSentences);
         return result;
     }
-
+    /**
+     * Asks user what to replace these lines with
+     * @param lineNodeArray
+     * @param lineNumber
+     * @param replacement
+     */
     public void replaceLine(Node[] lineNodeArray, int lineNumber, String replacement) {
+        System.out.println("Enter number of line you want to replace");
+        System.out.println("Insert new line");
         lineNodeArray[lineNumber - 1].setTextContent(replacement);
         System.out.println("The sentence has been replaced as follows:\n" +
                 lineNodeArray[lineNumber - 1].getTextContent() + "\nDo you want to save changes? (Y/N)");
+    
+	}
+    /**
+     * Overwrites current file with the play
+     * containing new, replaced lines
+     * @param newFilename
+     * @return true
+     */
+    public boolean saveFile(String newFilename) {
+        boolean success = true;
+        try {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            Result output = new StreamResult(new File(newFilename));
+            Source input = new DOMSource(this.document);
+            transformer.transform(input, output);
+        } catch (TransformerException e) {
+            e.printStackTrace();
+            success = false;
+        }
+        if (success) {
+            this.file = newFilename;
+        }
+
+        return success;
+
     }
 }
